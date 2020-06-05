3e5e4df3daf649cf84e04849e7803812f94994e5
Alexander-Koutrakis <61294700+Alexander-Koutrakis@users.noreply.github.com>
2020-06-05 16:52:06 +0300

    5/6/2020

---
+++ Assets/Scripts/HoverOutline.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+public class HoverOutline : MonoBehaviour , IPointerEnterHandler, IPointerExitHandler
+{
+    public RectTransform outlineRect;
+    public float scalingMultiplier;
+    private Vector3 startingScale;
+
+
+    private void Start()
+    {
+        startingScale = outlineRect.transform.localScale;
+    }
+   
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        LeanTween.scale(outlineRect, startingScale * scalingMultiplier, 0.3f);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        LeanTween.scale(outlineRect, startingScale, 0.5f);
+    }
+}
--- Assets/Scripts/PieGraph.cs
+++ Assets/Scripts/PieGraph.cs
@@ -2,19 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 public class PieGraph : MonoBehaviour
 {
     public float[] values;
+    public Sprite[] SDG_Sprites;
+    public Image[] TopSDGs=new Image[17];
     private float total;
     private Image[] images;
     private float zRotation;
-    PieText[] pieTexts;
+   
 
     private void Start()
     {
         GetValues();
         RefreshGraph();
-        pieTexts = GetComponentsInChildren<PieText>();
         GameMaster.gameMaster.pieGraph_Small = this;
     }
 
@@ -26,22 +28,52 @@ public class PieGraph : MonoBehaviour
         for (int i = 0; i < values.Length; i++)
         {
             total += values[i];
-
         }
-
         for (int i = 0; i < values.Length; i++)
-        {
-            
+        {           
             images[i].fillAmount = values[i]/total;
             images[i].transform.rotation = Quaternion.Euler(new Vector3(0, 0, zRotation));
             zRotation -= images[i].fillAmount*360f;
         }
 
+        GetTopSDGValues(values);
     }
 
     public void GetValues()
     {
         values = Player_Controller.player_Controller.SDGs;
         RefreshGraph();
+    }
+
+
+    private void GetTopSDGValues(float[] Sdgs) {
+        float top1 = 0;
+        float top2 = 0;
+        float top3 = 0;
+     
+
+        for(int i=0; i < Sdgs.Length; i++)
+        {
+            if (Sdgs[i] > top1)
+            {
+                top1 = Sdgs[i];               
+                TopSDGs[0].sprite = SDG_Sprites[i];
+                TopSDGs[0].transform.GetComponentInChildren<TextMeshProUGUI>().text = top1.ToString("F1");
+            } else if (Sdgs[i] > top2)
+            {
+                top2 = Sdgs[i];
+
+                TopSDGs[1].sprite = SDG_Sprites[i];
+                TopSDGs[1].transform.GetComponentInChildren<TextMeshProUGUI>().text = top2.ToString("F1");
+            } else if (Sdgs[i] > top3){
+                top3 = Sdgs[i];
+                TopSDGs[2].sprite = SDG_Sprites[i];
+                TopSDGs[2].transform.GetComponentInChildren<TextMeshProUGUI>().text = top3.ToString("F1");
+            }
+
+            
+        }
+
+
     }
 }
